312a028c1f3fe21ddbde20a379add412d58949d6
Brian Partridge <brianpartridge@gmail.com>
2013-09-25 22:24:47 -0400

    fix bugs in datasource observation
    - Previous impl allowed for duplicate observers on a single datasource because identity wasn't checked.
    - Move to NSHashTable with weak references to hold on to observers.

--- PTLDatasource/PTLDatasource.m
+++ PTLDatasource/PTLDatasource.m
@@ -9,26 +9,6 @@
 #import "PTLDatasource.h"
 #import <objc/runtime.h>
 
-#pragma mark - Observer Wrapper
-
-@interface WeakObserverWrapper : NSObject
-
-@property (nonatomic, weak) id<PTLDatasourceObserver>observer;
-
-+ (instancetype)wrapObserver:(id<PTLDatasourceObserver>)observer;
-
-@end
-
-@implementation WeakObserverWrapper
-
-+ (instancetype)wrapObserver:(id<PTLDatasourceObserver>)observer {
-    WeakObserverWrapper *wrapper = [[WeakObserverWrapper alloc] init];
-    wrapper.observer = observer;
-    return wrapper;
-}
-
-@end
-
 #pragma mark - PTLDatasource
 
 @interface PTLDatasource ()
@@ -81,15 +61,11 @@
 #pragma mark - PTLObservableDatasource
 
 - (void)addChangeObserver:(id<PTLDatasourceObserver>)observer {
-   [self.observers addObject:[WeakObserverWrapper wrapObserver:observer]];
+   [self.observers addObject:observer];
 }
 
 - (void)removeChangeObserver:(id<PTLDatasourceObserver>)observer {
-   NSArray *wrappersToRemove = [self.observers filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(WeakObserverWrapper *wrapper, NSDictionary *bindings) {
-      return (wrapper.observer == observer ||
-              wrapper.observer == nil);
-   }]];
-   [self.observers removeObjectsInArray:wrappersToRemove];
+   [self.observers removeObject:observer];
 }
 
 - (void)removeAllObservers {
@@ -100,43 +76,43 @@
 
 @dynamic observers;
 
-- (NSMutableArray *)observers {
-    NSMutableArray *result = objc_getAssociatedObject(self, @"observers");
+- (NSHashTable *)observers {
+    NSHashTable *result = objc_getAssociatedObject(self, @"observers");
     if (result == nil) {
-        result = [NSMutableArray array];
+        result = [NSHashTable weakObjectsHashTable];
         objc_setAssociatedObject(self, @"observers", result, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
     return result;
 }
 
 - (void)notifyObserversOfChangesBeginning {
-   for (WeakObserverWrapper *wrapper in self.observers) {
-      if ([wrapper.observer respondsToSelector:@selector(datasourceWillChange:)]) {
-         [wrapper.observer datasourceWillChange:self];
+   for (id observer in self.observers) {
+      if ([observer respondsToSelector:@selector(datasourceWillChange:)]) {
+         [observer datasourceWillChange:self];
       }
    }
 }
 
 - (void)notifyObserversOfChangesEnding {
-   for (WeakObserverWrapper *wrapper in self.observers) {
-      if ([wrapper.observer respondsToSelector:@selector(datasourceDidChange:)]) {
-         [wrapper.observer datasourceDidChange:self];
+   for (id observer in self.observers) {
+      if ([observer respondsToSelector:@selector(datasourceDidChange:)]) {
+         [observer datasourceDidChange:self];
       }
    }
 }
 
 - (void)notifyObserversOfChange:(PTLChangeType)change atIndexPath:(NSIndexPath *)indexPath newIndexPath:(NSIndexPath *)newIndexPath {
-   for (WeakObserverWrapper *wrapper in self.observers) {
-      if ([wrapper.observer respondsToSelector:@selector(datasource:didChange:atIndexPath:newIndexPath:)]) {
-         [wrapper.observer datasource:self didChange:change atIndexPath:indexPath newIndexPath:newIndexPath];
+   for (id observer in self.observers) {
+      if ([observer respondsToSelector:@selector(datasource:didChange:atIndexPath:newIndexPath:)]) {
+         [observer datasource:self didChange:change atIndexPath:indexPath newIndexPath:newIndexPath];
       }
    }
 }
 
 - (void)notifyObserversOfSectionChange:(PTLChangeType)change atSectionIndex:(NSInteger)sectionIndex {
-   for (WeakObserverWrapper *wrapper in self.observers) {
-      if ([wrapper.observer respondsToSelector:@selector(datasource:didChange:atSectionIndex:)]) {
-         [wrapper.observer datasource:self didChange:change atSectionIndex:sectionIndex];
+   for (id observer in self.observers) {
+      if ([observer respondsToSelector:@selector(datasource:didChange:atSectionIndex:)]) {
+         [observer datasource:self didChange:change atSectionIndex:sectionIndex];
       }
    }
 }
